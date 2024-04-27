3fd5cf1cc3d0dd11e3f3d21737bf48ab10480455
Dishani Navanjana <dishaninavanjana11@gmail.com>
2024-04-27 18:01:58 +0530

    agent ui created

---
+++ inc/agent-sidebar.php
@@ -0,0 +1,25 @@
+<nav class="sidebar">
+    <a href="agent-dashboard.php" class="menu-item">
+        <h3>Dashboard</h3>
+        <i class="fa-solid fa-chevron-right"></i>    
+    </a>
+
+    <a href="agent-user-profile.php" class="menu-item">
+        <h3>My Profile</h3>
+        <i class="fa-solid fa-chevron-right"></i>    
+    </a>
+
+    <a href="agent-coverage.php" class="menu-item">
+        <h3>Covrage</h3>
+        <i class="fa-solid fa-chevron-right"></i>
+    </a>
+
+    <a href="agent-add-customer.php" class="menu-item">
+        <h3>Add Customer</h3>
+        <i class="fa-solid fa-chevron-right"></i>
+    </a>
+    <a href="agent-reports.php" class="menu-item">
+        <h3>Reports</h3>
+        <i class="fa-solid fa-chevron-right"></i>
+    </a>
+</nav>
