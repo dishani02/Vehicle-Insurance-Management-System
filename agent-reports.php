1a1591e9b4eaf4fac59336b8f56aeb59c14ff9f3
Dishani Navanjana <dishaninavanjana11@gmail.com>
2024-04-27 21:19:53 +0530

    initial comit

--- agent-reports.php
+++ agent-reports.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard | Your Road to Safety and Savings</title>
     <link rel="stylesheet" href="css/style.css">
-        <!--font awesome-->
-        <script src="https://kit.fontawesome.com/72fb3712df.js" crossorigin="anonymous"></script>
+    <!--font awesome-->
+    <script src="https://kit.fontawesome.com/72fb3712df.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -20,7 +20,49 @@
             </div>
 
             <div class="content">
-                content here
+                <form action="post">
+                    <h2>Claim Intimation Form</h2>
+
+                    <div class="flex flex-col">
+                        <label for="">Name of the informant <span class="required">*</span></label>
+                        <input type="text" name="" placeholder="">
+                    </div>
+
+                    <div class="flex flex-col">
+                        <label for="">Insured's Name<span class="required">*</span></label>
+                        <input type="text" name="" placeholder="">
+                    </div>
+
+                    <div class="flex flex-col">
+                        <label for="">Type of the vehicle <span class="required">*</span></label>
+                        <input type="text" name="" placeholder="">
+                    </div>
+
+                    <div class="flex flex-col">
+                        <label for="">Date of the accident<span class="required">*</span></label>
+                        <input type="date" name="" placeholder="">
+                    </div>
+
+                    <div class="flex flex-col">
+                        <label for="">Place of the accident <span class="required">*</span></label>
+                        <input type="text" name="" placeholder="">
+                    </div>
+
+                    <div class="flex flex-col">
+                        <label for=""> Description of the accident<span class="required">*</span></label>
+                        <textarea name="" rows="4" cols="50"></textarea>
+                    </div>
+
+                    <div class="flex flex-col">
+                        <label for="">Add images<span class="required">*</span></label>
+                        <input type="File" name="" placeholder="Add a file">
+                    </div>
+
+                    <div class="flex" style="margin-top: 10px">
+                        <button type="submit" class="btn-primary">Submit</button>
+                        <button type="reset" class="btn-primary">Reset</button>
+                    </div>
+                </form>
             </div>
         </div>
     </div>
