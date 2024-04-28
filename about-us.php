3be2149b4713f9daba560f5bfaa0b06f6750cdcd
Dishani Navanjana <dishaninavanjana11@gmail.com>
2024-04-29 00:49:10 +0530

    fixes

--- about-us.php
+++ about-us.php
@@ -17,27 +17,63 @@
 
     <div class="container">
         <div class="slider">
-           
+            <h4>About Drive Peak</h4>
             <div class="owl-carousel">
-                <img src="https://www.srilankainsurance.com/assets/images/60th-anniversary-en.jpg" alt="" style="width: 100%">
+                <img src="https://www.srilankainsurance.com/assets/images/60th-anniversary-en.jpg" alt=""
+                    style="width: 100%">
                 <img src="https://www.srilankainsurance.com/assets/images/banner-4-en.jpg" alt="">
                 <img src="https://www.srilankainsurance.com/assets/images/home-protect-lite-banner-new.png" alt="">
             </div>
         </div>
-    </div>
 
-    <h1>Drive peak</h1>
-    <p>"DrivePeak is your trusted partner in vehicle insurance management.<br>Our cutting-edge platform harnesses the latest technology to deliver seamless,<br> efficient, and personalized solutions tailored to your needs With DrivePeak,<br> you can navigate insurance processes effortlessly, ensuring peace of mind and financial security on the road ahead."</p>
+        <div class="flex">
+            <div class="about">
+                <h1>Drive peak</h1>
+                <p>DrivePeak is your trusted partner in vehicle insurance management.<br>Our cutting-edge platform harnesses the
+                    latest technology to deliver<br>seamless efficient, and personalized solutions tailored to your needs<br>
+                    With DrivePeak,you can navigate insurance processes effortlessly,<br>ensuring peace of mind and financial
+                    security on the road ahead.</p>
+            </div>
+
+            <div class="vision">
+                <h1>Our Vision</h1>
+                <p>To envision a future where vehicle insurance management transcends mere protection, becoming a cornerstone of empowerment, trust, and seamless mobility for all.                </p>
+            </div>
+
+            <div class="mission">
+                <h1>Our Mission</h1>
+                    <p>Our mission is to revolutionize vehicle insurance management by leveraging cutting-edge technology, personalized service, and a relentless pursuit of innovation. Through transparent practices, proactive risk mitigation, and unwavering dedication to our customers, we aim to redefine the insurance experience, fostering safer roads, stronger communities, and greater peace of mind for drivers everywhere</p>
+            </div>
+        </div>
+
+        <div class="flex">
+            <div>
+                <h2>Our History</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tempora, officia, est, similique corporis recusandae quo dolore ullam asperiores et nesciunt dicta quasi. Autem totam at odit aliquid omnis id culpa!</p>
+            </div>
+            <div>
+                <h2>Awards</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam repudiandae modi eveniet rerum, quasi, sequi veniam voluptates vel alias illum neque odio! Rerum, eius. Placeat ullam eius quam dolor in.</p>
+            </div>
+            <div>
+                <h2>Our Ethors & Values</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit animi, ipsam quod est libero sit officiis dolor hic. Fugit maxime iusto atque ullam doloremque animi earum distinctio corporis qui tempora.</p>
+            </div>
+        </div>
+    </div>
+   
+
+    
 
     <?php require_once('inc/footer.php') ?>
-
+    </div>
     <!--jquery-->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <!--owl carousel-->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
     <!--script-->
     <script>
-        $(document).ready(function() {
+        $(document).ready(function () {
             $(".owl-carousel").owlCarousel({
                 loop: true,
                 margin: 10,
@@ -63,15 +99,14 @@
                 }
             });
         });
-     
-     
+
+
     </script>
 
-    
+
 
     <?php require_once('inc/footer.php') ?>
-    
+
 </body>
 
 </html>
-
