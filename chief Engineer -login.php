f48bf1681e1589f5731fc4e23e3bd1f286a7275d
randilkumal <randilkumal@gmail.com>
2024-05-02 15:12:12 +0530

    kumal update

---
+++ chief Engineer -login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chief Engineer Login | Your Road to Safety and Savings</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+<?php require_once('inc/header.php') ?>
+
+<div class="container">
+    <div class="flex">
+
+
+
+
+
+
+    
+        <?php require_once('inc/admin-sidebar.php') ?>
+</body>
+
+</html>
+    
