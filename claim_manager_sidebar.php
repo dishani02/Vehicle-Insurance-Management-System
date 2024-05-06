464658be2efc2901644bb1d19a051f86818041e4
Ashan Jayakody <jayakodyashan25@gmail.com>
2024-05-06 12:22:18 +0530

    claim manager dashboard

---
+++ claim_manager_sidebar.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang='en'>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Claims Manager|Dashboard</title>
+    <link rel="stylesheet" href="css/manager_styles.css">
+
+</head>
+
+<body>
+    <?php require_once("inc/header.php")?>
+
+    <div class="sidebar">
+        <nav>
+            <ul>
+                <li><a href="">Dashboard</a></li>
+                <li><a class ="active"href="">Customer list</a></li>
+                <li><a href="">Claims</a></li>
+                <li><a href="">Payments</a></li>
+            </ul>
+        </nav>
+    </div>
+    
+
+</body>
+</html>
